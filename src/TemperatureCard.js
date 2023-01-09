4e56359750d1f607853b801528b10610603c9067
Mac <mac@macs-mbp-2.guest.stockholm.se>
2023-01-09 19:51:32 +0100

    improved for netlify

--- src/TemperatureCard.js
+++ src/TemperatureCard.js
@@ -24,6 +24,8 @@ export default function TemperatureCard(props) {
                   <TemperatureDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
